6092c5d551e52699677ab007448e1fcc573b5fc2
ddhuu <ddhuu.dev@gmail.com>
2023-10-22 07:24:11 +0700

    Solve Coin Change with DP

---
+++ 08. Dynamic Programing/04_coinChange.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // Initialize for the empty subset.
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][amount];
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> coins = {1, 2, 5};
+    int amount = 5;
+    int result = solution.change(amount, coins);
+
+    cout << "The total number of distinct ways to make change is: " << result << endl;
+
+    return 0;
+}
+
--- 08. Dynamic Programing/README.md
+++ 08. Dynamic Programing/README.md
@@ -5,3 +5,5 @@ Given two strings of length m and n, find the length of their longest subsequenc
 Given two words word1 of length m and word2 of length n, find the minimum number of operations required to convert word1 to word2 in Time and Space Complexity O(mn). You have the following 3 operations permitted on a word: 1. Insert a character 2. Delete a character 3. Replace a character.
 ## 03. Sub Array sums equal K
 Given an array [] of length N and an integer Sum , the task is to find the number of subsets with equal to sum. 
+## 04. Coin Change
+Given an unlimited supply of coins of given denominations, find the total number of distinct ways to get a desired change in Time and Space Complexity in O(n^2).
